29ae769ba40f59e1c38cbeeecd4581ca8f1c4a50
Ines <ines.oliete@gmail.com>
2023-07-13 12:13:37 +0200

    Route new tweet

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/tweets');
 
 var app = express();
 
@@ -16,5 +17,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/tweets', tweetsRouter);
+
 //test
 module.exports = app;
