5ede16fdf21f48a1f241db80ababbe06fa7091ab
maximegenand <m.genand@gmx.fr>
2023-07-13 14:27:48 +0200

    update tweets routes

--- routes/tweets.js
+++ routes/tweets.js
@@ -2,16 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 require('../models/connection');
+const User = require('../models/users');
 const Tweet = require('../models/tweets');
 const { checkBody } = require('../modules/checkBody');
 
 router.post("/addtweet", async (req, res) => {
+  if (!checkBody(req.body, ['token', 'tweet'])) {
+    res.json({ result: false, error: 'Missing or empty fields' });
+    return;
+  }
+
   const data = await User.findOne({ token: req.body.token });
-  if (!data) {
+  //console.log(data);
+  if (data) {
     let newTweet = new Tweet({
       tweet: req.body.tweet,
       date: Date.now(),
-      user: data_id,
+      user: data._id,
     });
     await newTweet.save();
     res.json({ result: true });
