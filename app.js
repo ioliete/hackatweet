03811909b14a76ceb313ed14c3971ade27320ef1
maximegenand <m.genand@gmx.fr>
2023-07-13 14:11:06 +0200

    add dependencies & routes correction

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var usersRouter = require('./routes/tweets');
-var usersScreen = require('./routes/screens');
+require('./models/connection');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const tweetsRouter = require('./routes/tweets');
+const screensRouter = require('./routes/screens');
 
-var app = express();
+const app = express();
+
+const cors = require('cors');
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/tweets', tweetsRouter);
-app.use('/screens', tweetsRouter);
+app.use('/screens', screensRouter);
 
 
 module.exports = app;
